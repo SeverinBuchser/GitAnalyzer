6644fa1bd46cd0cfca3fb24e9dd1c774b8879f59
Severin Buchser <severin.buchser@gmx.ch>
2022-11-08 18:54:27 +0100

    Add README and ResolutionChunk

---
+++ src/main/java/ch/unibe/inf/seg/mergeresolution/resolution/ResolutionChunk.java
@@ -0,0 +1,32 @@
+package ch.unibe.inf.seg.mergeresolution.resolution;
+
+import org.eclipse.jgit.diff.RawText;
+import org.eclipse.jgit.merge.MergeChunk;
+
+import java.util.List;
+
+public class ResolutionChunk {
+    private final MergeChunk mergeChunk;
+    private final RawText sequence;
+
+    public ResolutionChunk(MergeChunk mergeChunk, RawText sequence) {
+        this.mergeChunk = mergeChunk;
+        this.sequence = sequence;
+    }
+
+    public String getString() {
+        return this.sequence.getString(
+            this.mergeChunk.getBegin(),
+            this.mergeChunk.getEnd(),
+            false
+        );
+    }
+
+    public static String concat(List<ResolutionChunk> chunks) {
+        StringBuilder builder = new StringBuilder();
+        for (ResolutionChunk resolutionChunk: chunks) {
+            builder.append(resolutionChunk.getString());
+        }
+        return builder.toString();
+    }
+}
